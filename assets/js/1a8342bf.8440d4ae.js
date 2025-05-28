4cb0d74920353af353152256d6f49fc76f0add5d
NabilSNaS <NabilSNaS@users.noreply.github.com>
2025-05-28 02:37:02 +0000

    deploy: 5de761013a4677af56a5b747f6ec4d5c990e47f5

---
+++ assets/js/1a8342bf.8440d4ae.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[7704],{3532:e=>{e.exports=JSON.parse('{"tags":[{"label":"blog","permalink":"/VAME/blog/tags/blog","count":1},{"label":"vame","permalink":"/VAME/blog/tags/vame","count":1}]}')}}]);
---
+++ assets/js/268dfc8f.a0fa7719.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[2996],{7827:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/VAME/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
